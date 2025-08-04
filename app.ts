6ae33518ccb553cd946be0daeb27940260d23183
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-08-04 17:13:00 -0300

    feat(routes): add base route structure for auth, user, and subscription

--- app.ts
+++ app.ts
@@ -2,7 +2,15 @@ import express, {Request, Response} from 'express'
 
 import { PORT } from './config/env';
 
+import userRouter from './routes/user.routes';
+import authRouter from './routes/auth.routes';
+import subscriptionRouter from './routes/subscriptions.routes';
+
 const app = express();
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
 
 app.get('/', (req: Request, res: Response) => {
    res.send('Voltrack API is live!')
