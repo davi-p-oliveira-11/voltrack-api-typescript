17ad3f21fdfa9afe6750f568321300cdfe19d3b2
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-08-05 10:58:27 -0300

    feat(middleware): add centralized error handling middleware

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import express, {Request, Response} from 'express'
+import cookieParser from 'cookie-parser'
 
 import { PORT } from './config/env';
 
@@ -6,12 +7,19 @@ import userRouter from './routes/user.routes';
 import authRouter from './routes/auth.routes';
 import subscriptionRouter from './routes/subscriptions.routes';
 import connectToDatabase from './database/mongodb';
+import errorMiddleware from './middlewares/error.middleware';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware)
 
 app.get('/', (req: Request, res: Response) => {
    res.send('Voltrack API is live!')
